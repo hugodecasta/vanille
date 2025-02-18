f19a33bfd9d59f5ca33e3e729018b60536ba9fa5
hugodecasta <hugo.castaneda.dev@gmail.com>
2025-02-19 00:03:27 +0100

    upd

--- promises.d.ts
+++ promises.d.ts
@@ -1,2 +1,3 @@
 export function wait_time(timer_ms: number): Promise<void>
 export function pending_promise(): [Function, Function, Promise<any>]
+export function set_iter_inter(iterations, timer_per_iteration, func): number
--- promises.js
+++ promises.js
@@ -1,10 +1,22 @@
 export async function wait_time(timer_ms) {
     return new Promise(ok => setTimeout(ok, timer_ms))
-} 
+}
 
 export function pending_promise() {
     let ok = null
     let reject = null
     let prom = new Promise((o, r) => { ok = o; reject = r })
     return [ok, reject, prom]
+}
+
+export function set_iter_inter(iterations, timer_per_iteration, func) {
+    let i = 0
+    let interval = setInterval(() => {
+        func(i)
+        i++
+        if (i >= iterations) {
+            clearInterval(interval)
+        }
+    }, timer_per_iteration)
+    return interval
 }
